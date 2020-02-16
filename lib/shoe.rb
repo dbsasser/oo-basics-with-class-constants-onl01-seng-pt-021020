22783dc375acb58ee56bb69e9f3ac4f84cd7ef72
Daniel Sasser <danielbsasser@gmail.com>
2020-02-16 03:06:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand 
+    end
+      
   end
 
   def cobble
